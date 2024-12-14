2547e10cec76be90aff5b6c4f91c861096ec0c15
PeijingX <peij.xu0@gmail.com>
2024-12-13 21:16:57 -0500

    error correction

--- lambda_calc.py
+++ lambda_calc.py
@@ -232,26 +232,26 @@ def calc_lamdot_acc_constraints(curve_js,lam,joint_vel_limit,joint_acc_limit,bre
 	return dlam_max,idx
 
 
-def calc_lamdot_dual(curve_js1,curve_js2,lam,joint_vel_limit1,joint_vel_limit2,step):
-	############find maximum lambda dot vs lambda
-	###curve_js1,2: curve expressed in joint space in radians for both robots
-	###lam: discrete lambda (relative path length), same shape as curve_js, from 0 to 1
-	###joint_vel_limit1,2: joint velocity limit in radians for both robots
-	###step: step size used for dense curve
+# def calc_lamdot_dual(curve_js1,curve_js2,lam,joint_vel_limit1,joint_vel_limit2,step):
+# 	############find maximum lambda dot vs lambda
+# 	###curve_js1,2: curve expressed in joint space in radians for both robots
+# 	###lam: discrete lambda (relative path length), same shape as curve_js, from 0 to 1
+# 	###joint_vel_limit1,2: joint velocity limit in radians for both robots
+# 	###step: step size used for dense curve
 
-	dlam_max=[]
+# 	dlam_max=[]
 
-	for i in range(0,len(lam)-step,step):
-		dq=np.abs(curve_js[i+step]-curve_js[i])
-		dqdlam=dq/(lam[i+step]-lam[i])
-		t=np.max(np.divide(dq,joint_vel_limit))
+# 	for i in range(0,len(lam)-step,step):
+# 		dq=np.abs(curve_js[i+step]-curve_js[i])
+# 		dqdlam=dq/(lam[i+step]-lam[i])
+# 		t=np.max(np.divide(dq,joint_vel_limit))
 
-		qdot_max=dq/t 		###approximated max qdot
-		dlam_max.append(qdot_max[0]/dqdlam[0])
+# 		qdot_max=dq/t 		###approximated max qdot
+# 		dlam_max.append(qdot_max[0]/dqdlam[0])
 
 
 
-	return dlam_max
+# 	return dlam_max
 
 
 def q_linesearch(alpha,qdot_prev,qdot_next,dt,joint_acc_limit):
@@ -619,5 +619,5 @@ def main():
 		
 
 
-if __name__ == "__main__":
-	main3()
+# if __name__ == "__main__":
+# 	main3()
